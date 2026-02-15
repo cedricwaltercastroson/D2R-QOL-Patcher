e43d4155bebc8863da305b28ad0295d82c69cfae
Cedric Walter C. Son <cedricwaltercastroson@hotmail.com>
2026-02-15 14:50:03 +1100

    Adjusted Patcher

--- patch.bat
+++ patch.bat
@@ -1,4 +1,4 @@
-set COWTEST=1
+set COWTEST=0
 REM Set COWTEST=0 to disable Cow test drops after testing
-if "%COWTEST%"=="1" (python patcher.py --vanilla "C:\vanilla" --out "C:\output" --cowtest) else (python patcher.py --vanilla "C:\vanilla" --out "C:\output")
+if "%COWTEST%"=="1" (python patcher.py --vanilla "C:\D2Rmod\vanilla" --out "C:\D2Rmod\output" --cowtest) else (python patcher.py --vanilla "C:\D2Rmod\vanilla" --out "C:\D2Rmod\output")
 pause
